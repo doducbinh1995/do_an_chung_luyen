4640814b8994db5494264ff30b8946ead6cc1993
doducbinh <doducbinh1995@gmail.com>
2020-09-28 22:47:08 +0700

    update

---
+++ doanchungluyen/src/js/danh-sach-tai-lieu-tham-khao.js
@@ -0,0 +1,8 @@
+export const Danh_Sach_Tai_Lieu_Tham_Khao = [{
+    Ten: 'Sổ Tay Quá Trình Và Thiết Bị Công Nghệ Hóa Chất',
+    Tac_Gia: ['GS,TSKH Nguyễn Bin', 'PGS,TS Đỗ Văn Đài', 'KS Long Thanh Hùng', 'TS Đinh Văn Huỳnh', 'PGS, TS Nguyễn Trọng Khuông', 'TS Phan Văn Thơm', 'TS Phạm Xuân Toàn', 'TS Trần Xoa'],
+    Noi_dung_tham_khao: [{
+        Noi_dung: 'Lý thuyết chưng luyện và hấp thụ',
+        Trang: '138'
+    }],
+}]
--- doanchungluyen/src/pages/phan-nam-danh-sach-tai-lieu-tham-khao/phan-nam-danh-sach-tai-lieu-tham-khao.view.jsx
+++ doanchungluyen/src/pages/phan-nam-danh-sach-tai-lieu-tham-khao/phan-nam-danh-sach-tai-lieu-tham-khao.view.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Danh_Sach_Tai_Lieu_Tham_Khao } from '../../js/danh-sach-tai-lieu-tham-khao';
+import './phan-nam-danh-sach-tai-lieu-tham-khao.scss';
 function TrangDanhSachThamKhao(props) {
     return (
-        <div></div>
+        <div>
+            {
+                Danh_Sach_Tai_Lieu_Tham_Khao.map((TaiLieu, indexTaiLieu) => <RenderChiTietTaiLieuThamKhao index={indexTaiLieu + 1} bookName={TaiLieu.Ten} listAuthor={TaiLieu.Tac_Gia} />)
+            }
+        </div>
     )
+}
+
+function RenderChiTietTaiLieuThamKhao(props) {
+    return <details>
+        <summary className="alert alert-primary" role="alert">{props.index}. {props.bookName}</summary>
+        <ul className="list-group">
+            {
+                props.listAuthor.map((authorName, indexData) => <li key={indexData} className="list-group-item">{authorName}</li>)
+            }
+        </ul>
+    </details>
 }
 
 export default TrangDanhSachThamKhao
